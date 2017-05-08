977f987bfbf76fbf3fbc8ab9cc38caafea51d68a
Danilo Moceri <danilom1993@gmail.com>
2017-05-08 11:02:04 +0200

    Prova atom

--- pgNetwork.vhd
+++ pgNetwork.vhd
@@ -1,8 +1,7 @@
-library ieee; 
-use ieee.std_logic_1164.all; 
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity pgNetwork is
-  
   generic (nBits : integer := 32);
   port (  A : in std_logic_vector(nBits-1 downto 0);
           B : in std_logic_vector(nBits-1 downto 0);
@@ -10,35 +9,35 @@ entity pgNetwork is
           P : out std_logic_vector(nBits-1 downto 0);
           G : out std_logic_vector(nBits-1 downto 0)
         );
-        
+
 end entity;
 
 architecture behavioral of pgNetwork is
-  
-  begin 
-    
+
+  begin
+
     proc : process (A, B) begin
-  
+
       for i in 0 to nBits-1 loop
-			
-		  if ( i = 0 ) then		
-			 
+
+		  if ( i = 0 ) then
+
         --if it is the first p g I have to take into account the carry input
         P(i) <= a(i) xor b(i);
 			  G(i) <= (a(i) and b(i)) or (a(i) and cin) or (b(i) and cin);
 			else
-      
+
 			 --generate the p g network
 				P(i) <= A(i) xor B(i);
 				G(i) <= A(i) and B(i);
 			end if;
-			
+
       end loop;
-    end process;    
+    end process;
 end architecture;
 
 configuration cfg_pgNetwork of pgNetwork is
-  
-  for behavioral 
+
+  for behavioral
   end for;
 end configuration;
